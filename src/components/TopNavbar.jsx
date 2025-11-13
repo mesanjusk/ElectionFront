db1ac054d66ce51c219527361794c9c00c5053a4
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-11-13 13:59:59 +0530

    1234

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -31,7 +31,7 @@ export default function TopNavbar({
         borderBottom: "1px solid rgba(15,23,42,0.08)",
       }}
     >
-      <Toolbar sx={{ justifyContent: "space-between", minHeight: 60 }}>
+      <Toolbar sx={{ justifyContent: "space-between", minHeight: 40 }}>
         {/* Left: Menu + Collection name + User name */}
         <Stack direction="row" spacing={1.5} alignItems="center">
           <IconButton
